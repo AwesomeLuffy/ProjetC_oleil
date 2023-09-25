0bad2c03f3bb2e168958ae761d955c3a9fbc92e4
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-09-25 09:04:46 +0200

    Move model.h to vue_controller.h

--- ProjetC_oleil/vue_controller/vue_controller.c
+++ ProjetC_oleil/vue_controller/vue_controller.c
@@ -1,6 +1,27 @@
-#include "../model/model.h"
+#include "vue_controller.h"
 #include <SDL.h>
 #include <stdio.h>
+
+/**
+* This function initialize some values related to the "Game" structure
+*
+* @return a game structure
+*/
+Game init() {
+    Game game;
+    // Create the window with the specified size, if we add "SDL_WINDOW_RESIZABLE" flags, it will allow to resize the window during the game
+    game.window = SDL_CreateWindow("Game test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 800, SDL_WINDOW_SHOWN);
+    // The renderer struct
+    game.render = SDL_CreateRenderer(game.window, -1, SDL_RENDERER_ACCELERATED);
+    // Initialize the event structure
+    game.event = malloc(sizeof(SDL_Event));
+    // To allow the loop to run we initialize it to true
+    game.isGameRunning = true;
+    // To not have a nullptr when we will want to use the gameObjects, we allocate memory for it
+    game.gameObjects = malloc(sizeof(GameObjects));
+    game.gameObjects->gameTitleBuffer = malloc(1024);
+    return game;
+}
 
 /**
 * This function is called before "the render function" each 16.7MS(60FPS), all content of the game have to be updated here(like for movement, collisions, etc.)
