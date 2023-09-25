08dfd889a915f837d5d18365221bc4f69d11f6c6
Selefin <105213277+Selefin@users.noreply.github.com>
2023-09-25 09:41:47 +0200

    Add files via upload

--- ProjetC_oleil/model/model.c
+++ ProjetC_oleil/model/model.c
@@ -1,1 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "model.h"
+
+Universe* initUniverse()
+{
+    printf("Game Initialisation\n");
+
+    FILE* file = fopen("../config.txt", "r");
+    if(file == NULL)
+    {
+        printf("Erreur lors de l'ouverture du fichier config.txt\n");
+        exit(1);
+    }
+
+    Universe* game = (Universe*)calloc(1, sizeof(Universe));
+
+    //Take the window size from the config.txt file
+    Coord WinSize;
+    char line[14] = {0};
+    fscanf(file, "%s %d %d", line, &WinSize.x, &WinSize.y);
+
+    //Take the start position from the config.txt file
+    fscanf(file, "%s %d %d", line, &game->start.x, &game->start.y);
+    if(game->start.x < 0 || game->start.x > WinSize.x || game->start.y < 0 || game->start.y > WinSize.y)
+    {
+        printf("Error in the config.txt file, closing program.\n");
+        free(game);
+        return NULL;
+    }
+
+    game->ship.pos = game->start;
+    game->ship.speed = 0;
+    game->ship.angle = 0;
+    game->ship.radius = 2;
+    game->ship.vectors = NULL;
+
+    fscanf(file, "%s %d %d", line, &game->end.x, &game->end.y);
+    if(game->end.x < 0 || game->end.x > WinSize.x || game->end.y < 0 || game->end.y > WinSize.y)
+    {
+        printf("Error in the config.txt file.\n");
+        free(game);
+        return NULL;
+    }
+
+    int nbSolarSystem = 0;
+    fscanf(file, "%s %d", line, &nbSolarSystem);
+    if(nbSolarSystem < 0)
+    {
+        printf("Error in the config.txt file.\n");
+        free(game);
+        return NULL;
+    }
+
+    game->solarSystem = (SolarSystem*)calloc(nbSolarSystem, sizeof(SolarSystem));
+    if(!game->solarSystem)
+    {
+        printf("Error while allocating memory for the solar system.\n");
+        free(game);
+        return NULL;
+    }
+
+    game->nbSolarSystem = nbSolarSystem;
+    for(int i = 0; i < nbSolarSystem; i++)
+    {
+        fscanf(file, "%s %d %d", line, &game->solarSystem[i].star.pos.x, &game->solarSystem[i].star.pos.y);
+        fscanf(file, "%s %d", line, &game->solarSystem[i].star.radius);
+
+        int nbPlanet = 0;
+        fscanf(file, "%s %d", line, &nbPlanet);
+        if(nbPlanet < 0)
+        {
+            printf("Error in the config.txt file.\n");
+            free(game);
+            return NULL;
+        }
+
+        game->solarSystem[i].planets = (Planet*)calloc(nbPlanet, sizeof(Planet));
+        if(!game->solarSystem[i].planets)
+        {
+            printf("Error while allocating memory for the planets.\n");
+            free(game);
+            return NULL;
+        }
+
+        game->solarSystem[i].nbPlanet = nbPlanet;
+        for(int j = 0; j < nbPlanet; j++)
+        {
+            fscanf(file, "%s %d", line, &game->solarSystem[i].planets[j].radius);
+            if(game->solarSystem[i].planets[j].radius < 0)
+            {
+                printf("Error in the config.txt file.\n");
+                free(game);
+                return NULL;
+            }
+
+            fscanf(file, "%s %d", line, &game->solarSystem[i].planets[j].orbit);
+            game->solarSystem[i].planets[j].pos.x = game->solarSystem[i].star.pos.x;
+            if(game->solarSystem[i].planets[j].orbit < 0)
+                game->solarSystem[i].planets[j].pos.y = game->solarSystem[i].star.pos.y - game->solarSystem[i].planets[j].orbit;
+            else 
+                game->solarSystem[i].planets[j].pos.y = game->solarSystem[i].star.pos.y + game->solarSystem[i].planets[j].orbit;
+        }
+    }
+
+    fclose(file);
+    return game;
+}
+
+void destroyGame(Universe* game)
+{
+    for(int i=0; i < game->nbSolarSystem; i++)
+    {
+        free(game->solarSystem[i].planets);
+    }
+    if(game->ship.vectors)
+        free(game->ship.vectors);
+    free(game->solarSystem);
+    free(game);
+}
+
+void universePrint(Universe* game)
+{
+    printf("\n\nGame :\n");
+    for(int i = 0; i < game->nbSolarSystem; i++)
+    {
+        printf("\nSolar System %d :\n", i+1);
+        printf("Star pos : %d %d\n", game->solarSystem[i].star.pos.x, game->solarSystem[i].star.pos.y);
+        printf("Star radius : %d\n", game->solarSystem[i].star.radius);
+        printf("Nb Planet : %d\n", game->solarSystem[i].nbPlanet);
+        for(int j = 0; j < game->solarSystem[i].nbPlanet; j++)
+        {
+            printf("  Planet %d :\n", j+1);
+            printf("  Planet radius : %d\n", game->solarSystem[i].planets[j].radius);
+            printf("  Planet orbit : %d\n", game->solarSystem[i].planets[j].orbit);
+            printf("  Planet pos : %d %d\n", game->solarSystem[i].planets[j].pos.x, game->solarSystem[i].planets[j].pos.y);
+        }
+    }
+}
+
+// int main()
+// {
+//     Universe* game = initUniverse();
+//     if(!game) { printf("Error while during the config file extraction, closing program.\n"); return 0; }
+//     printf("Game created\n");
+//     universePrint(game);
+//     destroyGame(game);
+//     printf("Done\n");
+//     return 0;
+// }
--- ProjetC_oleil/model/model.h
+++ ProjetC_oleil/model/model.h
@@ -1,1 +1,85 @@
 #pragma once
+
+/// @brief Structure to store a coordinate
+/// @param x The x coordinate
+/// @param y The y coordinate
+typedef struct Coord_s {
+    int x;
+    int y;
+} Coord;
+
+/// @brief Structure to store a vector
+/// @param force The force of the vector
+/// @param angle The angle of the vector
+typedef struct Vector_s {
+    int force;
+    int angle;
+} Vector;
+
+/// @brief Structure to store a planet
+/// @param pos The position of the planet
+/// @param radius The radius of the planet
+/// @param orbit The orbit of the planet
+typedef struct Planet_s {
+    Coord pos;
+    int radius;
+    int orbit;
+} Planet;
+
+/// @brief Structure to store a star
+/// @param pos The position of the star
+/// @param radius The radius of the star
+typedef struct Star_s {
+    Coord pos;
+    int radius;
+} Star;
+
+/// @brief Structure to store a solar system
+/// @param star The star of the solar system
+/// @param nbPlanet The number of planet in the solar system
+/// @param planets The planets of the solar system
+typedef struct SolarSystem_s {
+    Star star;
+    int nbPlanet;
+    Planet* planets;
+} SolarSystem;
+
+/// @brief Structure to store a ship
+/// @param pos The position of the ship
+/// @param radius The radius of the ship
+/// @param speed The speed of the ship
+/// @param angle The angle of the ship
+/// @param vectors The vectors of the ship
+typedef struct Ship_s {
+    Coord pos;
+    int radius;
+    int speed;
+    int angle;
+    Vector* vectors;
+} Ship;
+
+/// @brief Structure to store a game
+/// @param solarSystem The solar system of the game
+/// @param nbSolarSystem The number of solar system in the game
+/// @param ship The ship of the game
+/// @param start The start position of the ship
+/// @param end The position of the end of game
+typedef struct Universe_s {
+    SolarSystem* solarSystem;
+    int nbSolarSystem;
+    Ship ship;
+    Coord start;
+    Coord end;
+} Universe;
+
+/// @brief Function to destroy a game
+/// @param game The game to destroy
+void destroyGame(Universe* game);
+
+/// @brief Function to print a game in the terminal
+/// @param game The game to print
+void universePrint(Universe* game);
+
+/// @brief Function to initialize a game
+/// @return The game initialized
+Universe* initUniverse();
