32a12332c0bccd3866c6717935faeef0fa9bfe12
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-10-04 22:25:10 +0200

    Fix issue where some game values not correctly initialized

--- ProjetC_oleil/main.c
+++ ProjetC_oleil/main.c
@@ -1,25 +1,25 @@
 #include <SDL2/SDL.h>
 #include "vue_controller/vue_controller.h"
 
-
 int main(int argc, char *argv[])
 {
 
-    Game game;
+    Game *game;
 
     // Initialize SDL
     SDL_Init(SDL_INIT_VIDEO);
 
     // Init the window and the rendering
-    game = init();
+    init(game);
 
     // Run the game
-    run(&game);
+    run(game);
 
     // When game ended, destroy all
-    SDL_DestroyWindow(game.window);
-    SDL_DestroyRenderer(game.render);
+    SDL_DestroyWindow(game->window);
+    SDL_DestroyRenderer(game->render);
     SDL_Quit();
 
     return 0;
 }
+
