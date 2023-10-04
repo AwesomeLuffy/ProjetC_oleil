1f2e0b2c4861438a377c1c05c27e7d1479827d01
AwesomeLuffy <alpha.nexus.280@gmail.com>
2023-10-04 22:37:11 +0200

    Fixed merge issues

--- ProjetC_oleil/vue_controller/vue_controller.h
+++ ProjetC_oleil/vue_controller/vue_controller.h
@@ -1,8 +1,21 @@
 #pragma once
+
+#define true 1
+#define false 0
+
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include "../model/model.h"
 
+typedef struct Color {
+    SDL_Color* RED;
+    SDL_Color* GREEN;
+    SDL_Color* BLUE;
+    SDL_Color* WHITE;
+    SDL_Color* BLACK;
+    SDL_Color* YELLOW;
+    SDL_Color* CYAN;
+} Color;
 // Structure that contains all things related to handle the time of the game
 typedef struct Clock_s {
     double DELTA_TIME;
@@ -12,6 +25,8 @@ typedef struct Clock_s {
 // All objects that the game will contains (rectangle, planet, etc.)
 typedef struct GameContent_s {
     char* gameTitleBuffer;
+    SDL_Rect* rectangle;
+    Planet *planet1;
     Universe* universe;
 } GameObjects;
 
@@ -20,13 +35,16 @@ typedef struct Game_s {
     SDL_Window* window; // Actual Window that will contains the size and other parameters
     SDL_Renderer* render; // Renderering struct
     SDL_Event* event; // Allow to check if input from the user
-    bool isGameRunning;
+    int isGameRunning;
     GameObjects* gameObjects;
+    Color* color;
     Clock clock;
 } Game;
 
 // init the game (create the window and the rendering struct)
-Game init();
+void init(Game *game);
+void initColor(Color *color);
 void run(Game *game);
 void update(Game *game);
 void render(Game *game);
+int drawPlanet(SDL_Renderer *render, Planet *planet, int filled, SDL_Color *color);
